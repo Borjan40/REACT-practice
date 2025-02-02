0ef6d2987316f9170817b84d2efa8ea7edd6dedc
Borjan40 <andrey40cadrus@gmail.com>
2025-02-02 20:29:54 +0300

    feat: include React & SCSS in webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,21 +1,18 @@
-// import webpack from "webpack";
-
-// export function buildLoaders(): webpack.RuleSetRule[] {
-
-//     const typescriptLoader = {
-//         test: /\.tsx?$/,
-//         use: 'ts-loader',
-//         exclude: /node_modules/,
-//     }
-
-//     return [
-//         typescriptLoader,
-//     ]
-// }
-
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from js strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ]
+    }
 
     const typescriptLoader = {
         test: /\.tsx?$/,
@@ -25,6 +22,7 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
     return [
         typescriptLoader,
+        cssLoader
     ]
 }
 
